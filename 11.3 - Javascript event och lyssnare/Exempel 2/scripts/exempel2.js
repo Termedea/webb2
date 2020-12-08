b4a3d96f9066acf6bbde33707bc5799c1b879523
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2020-12-08 15:30:05 +0100

    11.3

---
+++ 11.3 - Javascript event och lyssnare/Exempel 2/scripts/exempel2.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("button-1");
+button.addEventListener("click", handleEvent);
+
+function handleEvent(event) {
+	console.log("Vi klickade på " +event.target.id);
+}
+
+
+var variabel = "Vi skriver bra kod";
+console.log("Vad hänbder? "+ variabel);
+
+
+
